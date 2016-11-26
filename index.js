6a23fee802c0351d1f86c2c9b43655b26fa972c0
bigmickeystyle <mike.123.smith@gmail.com>
2016-11-26 18:27:19 +0100

    heroku commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-    console.log('Express server is up on port 3000');
+app.listen(process.env.PORT || 3000, function () {
+    if (process.env.PORT) {
+        console.log('hi heroku');
+    } else {
+        console.log('Express server is up on port 3000');
+    }
 });
