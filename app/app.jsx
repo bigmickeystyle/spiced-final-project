f95cba6f4d54610dc82ab06c39fe165e09304240
bigmickeystyle <mike.123.smith@gmail.com>
2016-11-27 14:27:53 +0100

    added foundation and jquery

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,9 @@ var Player = require('Player');
 var About = require('About');
 var News = require('News');
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
       <Route path="/" component={Main}>
